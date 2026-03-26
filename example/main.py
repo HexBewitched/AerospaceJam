73d8df2263e26179b3cde5e5c9791933bba603f6
HexBewitched <theamazingmike111gmail.com>
2026-03-26 18:37:23 -0500

    added first sensor and integrated first server-app interaction, need to sync interaction between threads.

--- example/main.py
+++ example/main.py
@@ -1,12 +1,22 @@
 # These two modules allow us to run a web server.
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from bmp180 import BMP180
 # This module lets us pick random numbers, you can remove it later.
 import random
 
 # Here, we create the neccesary base app. You don't need to worry about this.
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+#sensor objects are defined here
+#bmp = BMP180() #TODO: uncomment when I have the drone
+
+#variables to handle button requests
+barometricPressureRequest = False
+
+#the dictionary for the data to be sent to the app
+dataDictionary = { }
 
 # When someone requests the root page from our web server, we return 'index.html'.
 @app.route('/')
@@ -15,19 +25,26 @@ def index():
 
 # This function runs in the background to transmit data to connected clients.
 def background_thread():
+    barometricPressureRequest = False
     while True:
         # We sleep here for a single second, but this can be increased or decreased depending on how quickly you want data to be pushed to clients.
         socketio.sleep(1)
+        
+        barometricPressure = random.randint(10,1000) #bmp.get_pressure() #TODO: uncomment when I have the drone
+        
+        #this is where we add data to the dictionary if it has been requested
+        if barometricPressureRequest == True:
+            print('send barometric pressure')
+            dataDictionary['barometricPressure'] = barometricPressure
+            barometricPressureRequest = False
+        dataDictionary['randomNumber'] = random.randint(1, 100)
+
         # Then, we emit an event called "update_data" - but this can actually be whatever we want - with the data being a dictionary
         # where 'randomNumber' is set to a random number we choose here. You should replace the data being sent back with your sensor data
         # that you fetch from things connected to your Pi.
         socketio.emit(
-            'update_data',
-            {
-                'randomNumber': random.randint(1, 100),
-                # you can add more here! for instance, something along the lines of:
-                # 'mySensor': mysensor.get_sensor_data(),
-            }
+            'update_data', #event name
+            dataDictionary #dictionary of data to send    
         )
         # To add a your first new sensor, try giving https://docs.aerospacejam.org/getting-started/first-sensor a read!
 
@@ -36,6 +53,11 @@ def background_thread():
 def handle_connect():
     print('Client connected')
     socketio.start_background_task(target=background_thread)
+
+#setter functions for the requests from the app
+@socketio.on('requstBarometricPressure')
+def requestBarometricPressure():
+    barometricPressureRequest = True #have this change sync across threads
 
 # This function is called
 def main():
